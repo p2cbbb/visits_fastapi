43ca7009195781c6a566d86ca06f33162cf787e0
Denis Antonov <den69ant@yandex.ru>
2022-10-04 18:10:09 +0300

    started working on /visited_domains endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
+from urllib.request import Request
 import aioredis
 from datetime import datetime
-from fastapi import FastAPI, status
+
+from fastapi import FastAPI, Request, status
+from typing import Union
+
 from schema import VisitedLinks
 
 app = FastAPI()
@@ -13,7 +17,7 @@ async def Home():
 
 
 @app.post("/visited_links", status_code=status.HTTP_201_CREATED)
-async def create_links(links: VisitedLinks):
+async def post_links(links: VisitedLinks):
     cur_time = int(datetime.now().timestamp())
     print(cur_time)
     print(links)
@@ -21,5 +25,17 @@ async def create_links(links: VisitedLinks):
     result = await redis.get('links')
     print(result)
     return {"status": "ok"}
+
+
+@app.get("/visited_domains", status_code=status.HTTP_200_OK)
+async def read_item(request: Request):
+    from_timestamp = request.query_params["from"]
+    to_timestamp = request.query_params["to"]
+    print(request.query_params["to"])
+    result = await redis.get('links')
+    print(result)
+    
+    return {"from": from_timestamp, "to": to_timestamp}
+
 
 
