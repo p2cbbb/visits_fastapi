4467cd2074d17f829ba77874edf7b7b277cfe3d8
Denis Antonov <den69ant@yandex.ru>
2022-10-07 08:11:52 +0300

    comments has been written in main.py

--- app/main.py
+++ app/main.py
@@ -14,24 +14,32 @@ redis = redis.StrictRedis(host='localhost', port=6379, db=0, charset="utf-8", de
 def post_links(links: VisitedLinks):
     cur_time = int(datetime.now().timestamp())
     visited_links = {}
+    # extract domains from list of urls and convert into set
     domains = set(map(extract_domain_from_url, links.links))
+    # convert set into string and devide it by "*"
+    # for add data into redis db
     visited_links[cur_time] = "*".join(domains)
     redis.mset(visited_links)
-    print(visited_links)
+    # print(visited_links)
     return {"status": "ok"}
 
 
 @app.get("/visited_domains", status_code=status.HTTP_200_OK)
 def get_domains(request: Request):
+    # template for response
     response = {
         "domains": []
     }
+    # get parameters from timestamp and to timestamp 
+    # then create a list with integers between these values
     from_timestamp = request.query_params["from"]
     to_timestamp = request.query_params["to"]
     range_timestamp = list(range(int(from_timestamp), int(to_timestamp)))
-    for t in range_timestamp:
+    # let's go through the list of timestamps and if it has domains
+    # add them into the template response
+    for time_int in range_timestamp:
         try:
-            domains = redis.mget(t)
+            domains = redis.mget(time_int)
             if domains != [None]:
                 response["domains"] = domains[0].split("*")
                 response["status"] = "ok"
