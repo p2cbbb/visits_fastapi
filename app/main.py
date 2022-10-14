3d6d9f4636c4e20e2624fb9903d101c70519ea28
Denis Antonov <den69ant@yandex.ru>
2022-10-14 09:38:03 +0300

    update redis into aioredis and the app was rewrite as an async program

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-import redis
+import aioredis
+
 from datetime import datetime
 from urllib.request import Request
 from fastapi import FastAPI, Request, status
@@ -7,12 +8,12 @@ from schema import VisitedLinks
 from utils import extract_domain_from_url
 
 app = FastAPI()
-redis = redis.StrictRedis(host='localhost', port=6379, db=0, charset="utf-8", decode_responses=True)
+redis = aioredis.from_url("redis://localhost", db=1)
 pipeline = redis.pipeline()
 
 
 @app.post("/visited_links", status_code=status.HTTP_201_CREATED)
-def post_links(links: VisitedLinks):
+async def post_links(links: VisitedLinks):
     cur_time = int(datetime.now().timestamp())
     visited_links = {}
     # extract domains from list of urls and convert into set
@@ -20,32 +21,41 @@ def post_links(links: VisitedLinks):
     # convert set into string and devide it by "*"
     # for add data into redis db
     visited_links[cur_time] = "*".join(domains)
-    pipeline.mset(visited_links)
-    pipeline.execute()
+    await pipeline.mset(visited_links)
+    await pipeline.execute()
     print(cur_time)
     return {"status": "ok"}
 
 
 @app.get("/visited_domains", status_code=status.HTTP_200_OK)
-def get_domains(request: Request):
+async def get_domains(request: Request):
     # template for response
     response = {
         "domains": []
     }
     # get parameters from timestamp and to timestamp 
     # then create a list with integers between these values
-    from_timestamp = request.query_params["from"]
-    to_timestamp = request.query_params["to"]
-    redis_keys = redis.keys()
+    from_timestamp = int(request.query_params["from"])
+    to_timestamp = int(request.query_params["to"])
+    # get all keys from redis
+    redis_keys = await redis.keys()
     # let's go through the list of keys and if it between timestamps
     # add them into the template response
     try:
         for key in redis_keys:
-            if key >= from_timestamp and key <= to_timestamp:
-                pipeline.mget(key)
-                domains = pipeline.execute()[0]
-                result_domains = domains[0].split("*")
-                response["domains"] = result_domains
+            if int(key) >= from_timestamp and int(key) <= to_timestamp:
+                # if key between timestamps get domain from pipeline
+                await pipeline.mget(key)
+                domains = await pipeline.execute()
+                # decode from bytes into string
+                domains_string = domains[0][0].decode()
+                # split several links by *
+                result_domains = domains_string.split("*")
+                # go through list of domains and if it does'nt in list of domains
+                # append it into response["domains"]
+                for domain in result_domains:
+                    if domain not in response["domains"]:
+                        response["domains"].append(domain)
                 response["status"] = "ok"
     except Exception as e:
         response["status"] = str(e)
