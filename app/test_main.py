7f97e30699ac71f899958324ca97a42d8f87f2b6
Denis Antonov <den69ant@yandex.ru>
2022-10-17 13:02:17 +0300

    tests was updated

--- app/test_main.py
+++ app/test_main.py
@@ -1,30 +1,30 @@
-from datetime import datetime
+import time
+
 from fastapi.testclient import TestClient
-
-from app.main import app
-
-client = TestClient(app)
-
-
-def test_create_links():
-    response = client.post(
-        "/visited_links",
-        headers={"X-Token": "coneofsilence"},
-        json={"links": ["https://ya.ru", "https://ya.ru?q=123", "funbox.ru", 
-        "https://stackoverflow.com/questions/11828270/how-to-exit-the-vim-editor"]},
-    )
-    assert response.status_code == 201
-    assert response.json() == {
-        "status": "ok"
-    }
+import json
+from main import app
+import os
 
 
-def test_get_domains():
-    cur_time = int(datetime.now().timestamp())
-    response = client.get(
-        f"/visited_domains?from={cur_time-1}&to={cur_time+1}",
-        headers={"X-Token": "coneofsilence"}
-    )
-    assert response.status_code == 200
-    assert response.json()["status"] == "ok"
-    assert "stackoverflow.com" and "funbox.ru" and "ya.ru" in response.json()["domains"]
+def test_post_request():
+    with TestClient(app) as client:
+        response = client.post('/visited_links', json={'links': ['ya.ru?q=123']})
+        assert response.status_code == 201
+        assert response.json()['status'] == 'ok'
+
+
+def test_data_saves_to_db():
+    with TestClient(app) as client:
+        current_time = int(time.time())
+        delta_time = 1000
+
+        post_request = client.post('/visited_links',
+                                   json={'links': ['ya.ru?q=123', 'funbox.com?q=1337', 'https://go.dev/solutions/']})
+        assert post_request.status_code == 200
+        assert post_request.json()['status'] == 'ok'
+
+        get_request = client.get(
+            f'/visited_domains?from_timestamp={current_time - delta_time}&to={current_time + delta_time}')
+        assert get_request.status_code == 200
+        assert get_request.json()['status'] == 'ok'
+        assert all([domain in get_request.json()['domains'] for domain in ['ya.ru', 'funbox.com', 'go.dev']])
